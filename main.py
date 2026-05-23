11d71b8cf888cb69c245954802a3fdbdf01a4985
Guillermo Ocaña <guillermoocaan@gmail.com>
2026-05-22 19:00:05 -0600

    minor change

--- main.py
+++ main.py
@@ -80,15 +80,15 @@ pi.set_PWM_frequency(PWM_PIN, 10000)
 pi.set_PWM_range(PWM_PIN, 255)
 
 print("25%")
-pi.set_PWM_dutycycle(PWM_PIN, 50)
+pi.set_PWM_dutycycle(PWM_PIN, 10)
 time.sleep(3)
 
 print("50%")
-pi.set_PWM_dutycycle(PWM_PIN, 70)
+pi.set_PWM_dutycycle(PWM_PIN, 15)
 time.sleep(3)
 
 print("75%")
-pi.set_PWM_dutycycle(PWM_PIN, 80)
+pi.set_PWM_dutycycle(PWM_PIN, 20)
 time.sleep(3)
 
 print("STOP")
