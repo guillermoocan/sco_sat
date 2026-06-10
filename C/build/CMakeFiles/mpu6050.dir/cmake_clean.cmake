5009ae52d91291dd5c3ba323f375986845c53a84
Guillermo Ocaña <guillermoocaan@gmail.com>
2026-06-10 17:37:43 -0600

    C1

---
+++ C/build/CMakeFiles/mpu6050.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpu6050.dir/src/mpu6050.c.o"
+  "CMakeFiles/mpu6050.dir/src/mpu6050.c.o.d"
+  "libmpu6050.a"
+  "libmpu6050.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mpu6050.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ C/build/CMakeFiles/sat.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sat.dir/link.d"
+  "CMakeFiles/sat.dir/src/main.c.o"
+  "CMakeFiles/sat.dir/src/main.c.o.d"
+  "CMakeFiles/sat.dir/src/mpu6050.c.o"
+  "CMakeFiles/sat.dir/src/mpu6050.c.o.d"
+  "CMakeFiles/sat.dir/src/qmc5883l.c.o"
+  "CMakeFiles/sat.dir/src/qmc5883l.c.o.d"
+  "sat"
+  "sat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
