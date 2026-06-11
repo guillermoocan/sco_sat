ccf37af06920c464d9868d0c2d095ab15809aafd
Guillermo Ocaña <guillermoocaan@gmail.com>
2026-06-10 22:36:49 -0600

    C_1

--- C/src/main.c
+++ C/src/main.c
@@ -38,19 +38,21 @@ int main(void)
 
     printf("QMC5883L detectado\n");
 
-    while (1)
-    {
-        MPU6050_Read(&imu);
-        QMC5883L_Read(&mag);
+    QMC5883L_Calibrate(&mag, "calibration.csv", 1000, 10000);
 
-        printf(" ACC: %.3f %.3f %.3f | GYRO: %.3f %.3f %.3f | MAG: %.3f %.3f %.3f | |B|: %.3f\n",
-            imu.accel[0], imu.accel[1], imu.accel[2],
-            imu.gyro[0], imu.gyro[1], imu.gyro[2],
-            mag.field[0], mag.field[1], mag.field[2],
-            mag.field_mag);
+    // while (1)
+    // {
+    //     MPU6050_Read(&imu);
+    //     QMC5883L_Read(&mag);
 
-        usleep(10000);
-    }
+    //     printf(" ACC: %.3f %.3f %.3f | GYRO: %.3f %.3f %.3f | MAG: %.3f %.3f %.3f | |B|: %.3f\n",
+    //         imu.accel[0], imu.accel[1], imu.accel[2],
+    //         imu.gyro[0], imu.gyro[1], imu.gyro[2],
+    //         mag.field[0], mag.field[1], mag.field[2],
+    //         mag.field_mag);
+
+    //     usleep(10000);
+    // }
 
     return 0;
 }
--- C/src/qmc5883l.c
+++ C/src/qmc5883l.c
@@ -8,13 +8,12 @@
 #include <sys/ioctl.h>
 #include <math.h>
 
-static float D[3][3] =
-    {
-        {0.9854f, -0.0133f, -0.0172f},
-        {-0.0133f, 1.0653f, 0.0009f},
-        {-0.0172f, 0.0009f, 0.9531f}};
-
+static float D[3][3] ={{0.9854f, -0.0133f, -0.0172f},{-0.0133f, 1.0653f, 0.0009f}, {-0.0172f, 0.0009f, 0.9531f}};
 static float b[3] = {0.1738f, -0.0701f, -0.2817f};
+
+static float D[3][3] ={{0.9854f, -0.0133f, -0.0172f},{-0.0133f, 1.0653f, 0.0009f}, {-0.0172f, 0.0009f, 0.9531f}};
+static float b[3] = {0.1738f, -0.0701f, -0.2817f};
+
 
 static int QMC5883L_WriteReg(QMC5883L_Type *qmc, uint8_t reg, uint8_t value)
 {
@@ -132,7 +131,16 @@ int QMC5883L_Calibrate(QMC5883L_Type *qmc, const char *filename, uint32_t sample
 
     for (uint32_t i = 0; i < samples; i++)
     {
-        QMC5883L_Read(qmc);
+        if (QMC5883L_ReadRegs(qmc, QMC5883L_DATA_OUTPUT_X_LSB, qmc->field_raw, 6))
+            return;
+
+        qmc->field_raw_16[0] = (int16_t)((qmc->field_raw[1] << 8) | qmc->field_raw[0]);
+        qmc->field_raw_16[1] = (int16_t)((qmc->field_raw[3] << 8) | qmc->field_raw[2]);
+        qmc->field_raw_16[2] = (int16_t)((qmc->field_raw[5] << 8) | qmc->field_raw[4]);
+
+        qmc->field[0] = (float)qmc->field_raw_16[0] 
+        qmc->field[1] = (float)qmc->field_raw_16[1] 
+        qmc->field[2] = (float)qmc->field_raw_16[2] 
 
         fprintf(fp, "%.8f,%.8f,%.8f\n",
                 qmc->field[0],
